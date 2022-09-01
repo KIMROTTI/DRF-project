04c4f38c0a7897b5423a7a97759c7916af55aa15
Rotti_Kim <andy_1009@naver.com>
2022-09-01 18:16:43 +0900

    first commit

---
+++ drf/frontend/src/App.js
@@ -0,0 +1,15 @@
+import RestAPI from "./RestAPI.js";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RestAPI />
+      </header>
+    </div>
+  );
+}
+
+export default App;
